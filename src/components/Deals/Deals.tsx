7d896af4e36d743f2afff336082f11143ca94f6d
vitalii-pc <alievruslan1818@gmail.com>
2025-02-20 16:43:32 +0100

    add swiper

--- src/components/Deals/Deals.tsx
+++ src/components/Deals/Deals.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import Swiper from "swiper";
+import "swiper/swiper-bundle.min.css";
+
 import s from "./Deals.module.css";
 import listDeal from "./listDeal.json";
 
 export default function Deals() {
+	useEffect(() => {
+		const swiper = new Swiper(".mySwiper", {
+			slidesPerView: 4,
+			spaceBetween: 20,
+			freeMode: true,
+			scrollbar: {
+				el: ".swiper-scrollbar",
+				hide: true,
+			},
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 10,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+			},
+		});
+
+		return () => {
+			swiper.destroy();
+		};
+	}, []);
+
 	return (
 		<section className={s.pastTrades}>
 			<div className={`container ${s.tradesContainer}`}>
-				<h2 className={`title ${s.tradeTitle}`}>минулі угоди</h2>
+				<h2 className={`title ${s.tradeTitle}`}>Прошедшие сделки</h2>
 				<p className={s.tradeStatus}>Онлайн</p>
 				<div className={`${s.swiper} ${s.mySwiper}`}>
 					<div className={`${s.swiperWrapper} ${s.wrapperCard}`}>
@@ -14,21 +44,21 @@ export default function Deals() {
 							{listDeal.map((list) => (
 								<li key={list.id} className={`${s.swiperSlide} ${s.card}`}>
 									<div className={s.nearSpot}>
-										<h3 className="sub-title">{list.currency}</h3>
+										<h3 className={s.subTitle}>{list.currency}</h3>
 										<p className={`${s.tradeText} ${s.opacityFirst}`}>
-											SPOT · 1 хв. тому
+											SPOT · 1 мин. назад
 										</p>
 									</div>
 									<div className={s.profitPercent}>
 										<p
 											className={`${s.tradeText} ${s.tradeProfit} ${s.opacitySecond}`}
 										>
-											ПРИБУТОК
+											Прибыль
 										</p>
 										<p className={s.tradePercent}>
 											{list.procent}%
-											<svg className="trade-icon">
-												<use href="./sprite.svg#arrow-top"></use>
+											<svg className={s.tradeIcon}>
+												<use href="/sprite.svg#arrow-top"></use>
 											</svg>
 										</p>
 									</div>
@@ -39,7 +69,7 @@ export default function Deals() {
 										<p
 											className={`${s.tradeText} ${s.opacityFirst} ${s.tradeWeight}`}
 										>
-											Дата входу {list.date}
+											Дата входа {list.date}
 										</p>
 									</div>
 								</li>
