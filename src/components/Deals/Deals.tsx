4313dc1ac74d9fb4c08ef6b81dcc420e35aca02b
vitalii-pc <alievruslan1818@gmail.com>
2025-02-20 16:09:16 +0100

    add styles

--- src/components/Deals/Deals.tsx
+++ src/components/Deals/Deals.tsx
@@ -4,35 +4,41 @@ import listDeal from "./listDeal.json";
 
 export default function Deals() {
 	return (
-		<section className="past-trades">
-			<div className="container trades-container">
-				<h2 className="title trade-title">минулі угоди</h2>
-				<p className="trade-status">Онлайн</p>
-				<div className="swiper mySwiper">
-					<div className="swiper-wrapper wrapper-card">
-						<ul className="swiper-wrapper wrapper-card">
+		<section className={s.pastTrades}>
+			<div className={`container ${s.tradesContainer}`}>
+				<h2 className={`title ${s.tradeTitle}`}>минулі угоди</h2>
+				<p className={s.tradeStatus}>Онлайн</p>
+				<div className={`${s.swiper} ${s.mySwiper}`}>
+					<div className={`${s.swiperWrapper} ${s.wrapperCard}`}>
+						<ul className={`${s.swiperWrapper} ${s.wrapperCard}`}>
 							{listDeal.map((list) => (
-								<li key={list.id} className="swiper-slide card">
-									<div className="near-spot">
+								<li key={list.id} className={`${s.swiperSlide} ${s.card}`}>
+									<div className={s.nearSpot}>
 										<h3 className="sub-title">{list.currency}</h3>
-										<p className="trade-text opacity-first">
+										<p className={`${s.tradeText} ${s.opacityFirst}`}>
 											SPOT · 1 хв. тому
 										</p>
 									</div>
-									<div className="profit-percent">
-										<p className="trade-text trade-profit opacity-second">
+									<div className={s.profitPercent}>
+										<p
+											className={`${s.tradeText} ${s.tradeProfit} ${s.opacitySecond}`}
+										>
 											ПРИБУТОК
 										</p>
-										<p className="trade-percent">
+										<p className={s.tradePercent}>
 											{list.procent}%
 											<svg className="trade-icon">
-												<use href="./img/sprite.svg#arrow-top"></use>
+												<use href="./sprite.svg#arrow-top"></use>
 											</svg>
 										</p>
 									</div>
-									<div className="goal-date">
-										<p className="trade-text opacity-second">{list.goal}</p>
-										<p className="trade-text opacity-first trade-weight">
+									<div className={s.goalDate}>
+										<p className={`${s.tradeText} ${s.opacitySecond}`}>
+											{list.goal}
+										</p>
+										<p
+											className={`${s.tradeText} ${s.opacityFirst} ${s.tradeWeight}`}
+										>
 											Дата входу {list.date}
 										</p>
 									</div>
@@ -41,7 +47,7 @@ export default function Deals() {
 						</ul>
 					</div>
 				</div>
-				<div className="swiper-scrollbar"></div>
+				<div className={s.swiperScrollbar}></div>
 			</div>
 		</section>
 	);
