4aa6b79eb00e4a66a35fcdffeeed1341e0050fa9
vitalii-pc <alievruslan1818@gmail.com>
2025-02-20 13:16:17 +0100

    add section Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
+import Profits from "@/components/Profits/Profits";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<Profits />
 		</>
 	);
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,66 @@
+"use client";
 import s from "./Hero.module.css";
+import { motion } from "framer-motion";
+import {
+	slideInFromBot,
+	slideInFromLeft,
+	slideInFromRight,
+} from "@/components/UI/motion/motion";
 
 const Hero = () => {
 	return (
-		<div className={`section ${s.Hero__section}`}>
-			<div className="container">Hero</div>
-		</div>
+		<section className={`section ${s.heroSection}`}>
+			<div className="container">
+				<div className={s.imgTitle}>
+					<motion.div
+						exit="exit"
+						initial="hidden"
+						animate="visible"
+						variants={slideInFromBot(1)}
+						className={s.animateDesktop}
+					>
+						<picture className={`${s.heroImg} ${s.animationImg}`}>
+							<source
+								media="(min-width: 1200px)"
+								srcSet="./img-hero-desk.webp 1x, ./img-hero-desk@2x.webp 2x"
+								type="image/webp"
+							/>
+							<source
+								media="(min-width: 1200px)"
+								srcSet="./img-hero-desk.png 1x, ./img-hero-desk@2x.png 2x"
+								type="image/png"
+							/>
+							<img
+								src="./img-hero-desk.png"
+								alt="сделки професиональных трейдеров"
+							/>
+						</picture>
+					</motion.div>
+
+					<div className={s.wrapperText}>
+						<h1 className={s.heroTitle}>
+							моментально копируй сделки профи трейдеров
+						</h1>
+						<p className={s.heroText}>
+							Начни копировать сделки с успешной командой профессиональных
+							трейдеров в автоматическом режиме.
+						</p>
+						<form name="form_hero" className={s.heroForm}>
+							<input
+								type="email"
+								name="email"
+								className={s.inputEmail}
+								placeholder="Ваш e–mail"
+							/>
+							<button type="submit" className={`button ${s.buttonForm}`}>
+								почати
+							</button>
+						</form>
+						<p className={s.afterForm}>5 дней бесплатного пользования</p>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
 
