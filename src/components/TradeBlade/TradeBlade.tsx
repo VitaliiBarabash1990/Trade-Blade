1d65bb8a82a8a8b3b0a578e433581473e5edce2e
vitalii-pc <alievruslan1818@gmail.com>
2025-02-21 22:38:35 +0100

    add tradeblade

--- src/components/TradeBlade/TradeBlade.tsx
+++ src/components/TradeBlade/TradeBlade.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import s from "./TradeBlade.module.css";
+import trades from "./Trades.json";
+import { li } from "framer-motion/client";
 
 export default function TradeBlade() {
 	return (
@@ -10,9 +12,23 @@ export default function TradeBlade() {
 						<h2 className={`title ${s.tradeTitle}`}>tradeblade это</h2>
 					</li>
 					<li className={s.right}>
-						<svg className={s.tradeIcon}>
-							<use href="/sprite.svg#icon-frame_1"></use>
-						</svg>
+						<ul className={s.rightItems}>
+							{trades.map((trade) => (
+								<li className={s.rightList}>
+									<svg className={s.tradeIcon}>
+										<use href={trade.src}></use>
+									</svg>
+									<div className={s.rightListWrap}>
+										<h3 className={`title ${s.rightListTitle}`}>
+											{trade.title}
+										</h3>
+										<p className={`p ${s.rightListText}`}>
+											{trade.description}
+										</p>
+									</div>
+								</li>
+							))}
+						</ul>
 					</li>
 				</ul>
 			</div>
