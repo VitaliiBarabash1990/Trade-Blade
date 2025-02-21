aff670a01ef65de01d1f40949248ef0752c32046
vitalii-pc <alievruslan1818@gmail.com>
2025-02-21 23:17:53 +0100

    add rectangle in useservise

--- src/components/UseServise/UseServise.tsx
+++ src/components/UseServise/UseServise.tsx
@@ -7,6 +7,10 @@ export default function UseServise() {
 		<section id="Use" className={`section ${s.sectionUse}`}>
 			<div className="container">
 				<ul className={s.useWraper}>
+					<svg className={s.useRectangle}>
+						<use href="/sprite.svg#icon-Rectangle-purple"></use>
+					</svg>
+
 					<li className={s.useItem}>
 						<h2 className={`title ${s.useTitle}`}>
 							Как начать пользоваться сервисом
@@ -33,4 +37,30 @@ export default function UseServise() {
 			</div>
 		</section>
 	);
+}
+
+{
+	/* <div className={s.wrapperImg}>
+	<picture>
+		<source
+			media="(min-width: 1280px)"
+			srcSet="
+                    ./profit-desctop.png    1x,
+                    ./profit-desctop@2x.png 2x
+                  "
+			type="image/png"
+		/>
+		<source
+			media="(min-width: 375px)"
+			srcSet="
+                    ./title-profit.png    1x,
+                    ./title-profit@2x.png 2x
+                  "
+			type="image/png"
+		/>
+
+		<img src="./title-profit.png" alt="фон для заголовка" />
+	</picture>
+	<h2 className={s.title}>О компании</h2>
+</div>; */
 }
